4a636fb398dc8d055a16173ae742132aee86213a
John Karima <karimajohn24@gmail.com>
2020-09-06 13:15:54 +0300

    create a User and Credentials class

---
+++ password.py
@@ -0,0 +1,13 @@
+class User:
+    """
+    Class that generates new instances of users
+    """
+
+    pass
+
+class Credentials:
+    """
+    Class that generates new instances of credentials
+    """
+
+    pass
