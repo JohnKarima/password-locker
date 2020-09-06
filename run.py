dc4c5c9b7fd334af6b16179f65389e9c9e7762e6
John Karima <karimajohn24@gmail.com>
2020-09-06 14:27:55 +0300

    create fxn to create a new credential

--- run.py
+++ run.py
@@ -1,2 +1,10 @@
-#! shebang
 #!/usr/bin/env python3.8
+from password import Credentials
+
+def create_credentials(username, password):
+    '''
+    function to create  a new credential
+    '''
+    new_credential = Credentials(username,password)
+    return new_credential
+    
